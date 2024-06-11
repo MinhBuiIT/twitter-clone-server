ae48992a624ae219a7dfe2054dd9a2009e255a50
Bui Kha Minh <buikhaminh2003@gmail.com>
2024-06-11 11:33:05 +0700

    Update v6

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,19 +1,18 @@
 import { config } from 'dotenv'
-import fs from 'fs'
-import path from 'path'
 
-const env = process.env.NODE_ENV
-if (!env) {
-  console.log('Vui lòng truyền NODE_ENV như production hoặc deveplopment')
-  process.exit(1)
-}
-const fileEnvName = `.env.${env}`
-if (!fs.existsSync(path.resolve(fileEnvName))) {
-  console.log(`Vui lòng tạo file ${fileEnvName}, nếu ở môi trường dev thì tạo file .env.development`)
-  process.exit(1)
-}
-console.log(`Bạn đang chạy dự án ở môi trường ${env} ở file ${fileEnvName}`)
+// const env = process.env.NODE_ENV
+// if (!env) {
+//   console.log('Vui lòng truyền NODE_ENV như production hoặc deveplopment')
+//   process.exit(1)
+// }
+// const fileEnvName = `.env.${env}`
+// if (!fs.existsSync(path.resolve(fileEnvName))) {
+//   console.log(`Vui lòng tạo file ${fileEnvName}, nếu ở môi trường dev thì tạo file .env.development`)
+//   process.exit(1)
+// }
+// console.log(`Bạn đang chạy dự án ở môi trường ${env} ở file ${fileEnvName}`)
 
-config({
-  path: fileEnvName
-})
+// config({
+//   path: fileEnvName
+// })
+config()
